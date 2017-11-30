9036a1f147a1d8b30815db96f43ce4c668466808
HuyNhatNgo232 <nhathuy1432@gmail.com>
2017-11-30 14:28:23 -0800

    update

--- folder/urls.py
+++ folder/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='folder_list'),
     url(r'^folder/(?P<pk>\d+)$', views.FolderDetailView.as_view(), name='folder_detail'),
-
+    # create new folder
+    url(r'^folder/new/$', views.NewFolderView.as_view(), name='folder_new'),
+    # edit folder
+    url(r'^folder/(?P<pk>\d+)/edit/$', views.EditFolderView.as_view(), name='folder_edit'),
+    # delete folder
+    url(r'^post/(?P<pk>\d+)/remove/$', views.DeleteFolderView.as_view(), name='folder_remove'),
 ]
--- folder/views.py
+++ folder/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from folder.models import Folder
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
@@ -17,3 +19,36 @@ class FolderDetailView(DetailView):
     model = Folder
     template_name = 'folder/folder_detail.html'
     context_object_name = 'folder'
+
+class MyFolder(IndexView):
+    def get_queryset(self):
+        print "something"
+        return Folder.objects.filter(created_by=self.request.user.id)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        print "dispatch"
+        return super(MyFolder, self).dispatch(*args, **kwargs)
+
+
+class NewFolderView(CreateView):
+    model = Folder
+    fields = ['title', ]
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super(NewFolderView, self).form_valid(form)
+
+
+class EditFolderView(UpdateView):
+    model = Folder
+    fields = ['title', ]
+
+    def get_queryset(self):
+        base_qs = super(EditFolderView, self).get_queryset()
+        return base_qs.filter(created_by=self.request.user)
+
+
+class DeleteFolderView(DeleteView):
+    model = Folder
+    success_url = reverse_lazy('folder_list')
